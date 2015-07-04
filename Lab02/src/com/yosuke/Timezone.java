eb4a328899cd3a9cf907edcecaf33c5729a15b75
yosuke <yosuke42551@gmail.com>
2015-07-04 17:16:43 +0800

    Change name to Timezone
    change name

---
+++ Lab02/src/com/yosuke/Timezone.java
@@ -0,0 +1,22 @@
+package com.yosuke;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+public class Timezone {
+
+	public static void main(String[] args) {
+		
+		String x = "Asia/Tokyo";
+		TimeZone tz = TimeZone.getTimeZone(x);  
+		TimeZone.setDefault(tz);
+		Date z = new Date();
+		String[] p = x.split("/");
+		SimpleDateFormat d = new SimpleDateFormat("yyyy年MM月dd日 a HH時mm分ss秒");
+		
+		System.out.println(p[1]+"現在的時間為："+d.format(z));
+
+	}
+
+}
